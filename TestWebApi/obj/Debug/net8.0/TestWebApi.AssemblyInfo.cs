180b2bbd47fcd23c0cdb6662cc6981ef08a333cf
Sahil_Wazade <swazade@provepartners.com>
2025-01-06 18:59:20 +0530

    removed unnecessary files

--- TestWebApi/obj/Debug/net8.0/TestWebApi.AssemblyInfo.cs
+++ TestWebApi/obj/Debug/net8.0/TestWebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7c358ff16e6b1ecedda3542c452b4fefab6cac9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
